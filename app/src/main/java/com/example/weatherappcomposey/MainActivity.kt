678b291d1a71a4f3f722418c7b2758b4aea4e86e
Haggai Khafizov <hafyok5777@gmail.com>
2023-07-18 14:52:50 +0300

    Response data from weatherapi.com with volley

--- app/src/main/java/com/example/weatherappcomposey/MainActivity.kt
+++ app/src/main/java/com/example/weatherappcomposey/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.weatherappcomposey
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,6 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherappcomposey.screens.MainCard
 import com.example.weatherappcomposey.screens.TabLayout
 import com.example.weatherappcomposey.ui.theme.WeatherAppComposeYTheme
@@ -21,6 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppComposeYTheme {
+                getData("London", this)
                 Image(
                     painter = painterResource(id = R.drawable.sky),
                     contentDescription = "im1",
@@ -36,4 +42,25 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun getData(city:String, context: Context){
+    val url = "https://api.weatherapi.com/v1/current.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+            response ->
+            Log.d("MyLog", "Response: $response")
+        },
+        {
+            Log.d("MyLog", "VolleyError: $it")
+        }
+    )
+    queue.add(sRequest)
 }
